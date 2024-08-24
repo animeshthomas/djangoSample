1e4cd7429314cb54e006a08d1c17c91732f067b8
Animesh Thomas <animeshthomas262@gmail.com>
2024-08-24 16:22:25 +0530

    redirection done

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the sampleapp index.")
+    return render(request, 'index.html')
 
 
 # Create your views here.
